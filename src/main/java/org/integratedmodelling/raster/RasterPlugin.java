b70881f82b4d5758ec1471bd234bf85c42790b25
Íñigo Cobián <inigo.cobian@gmail.com>
2025-06-20 13:53:26 +0200

    Create structure of the project

---
+++ src/main/java/org/integratedmodelling/raster/RasterPlugin.java
@@ -0,0 +1,14 @@
+package org.integratedmodelling.raster;
+
+import org.pf4j.PluginWrapper;
+
+public class RasterPlugin {//extends KlabComponent {
+
+    public RasterPlugin(PluginWrapper wrapper) {
+        super();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hi, raster!");
+    }
+}
---
+++ src/main/java/org/integratedmodelling/raster/adapters/RasterAdapter.java
@@ -0,0 +1,9 @@
+package org.integratedmodelling.raster.adapters;
+
+import org.integratedmodelling.klab.api.data.Version;
+import org.integratedmodelling.klab.api.services.resources.adapters.ResourceAdapter;
+
+@ResourceAdapter(name = "raster", universal = true, version = Version.CURRENT)
+public class RasterAdapter {
+
+}
