3f87a3c8d1a0058ef784d0728fca3b5b4a5c8c98
Íñigo Cobián <inigo.cobian@gmail.com>
2025-06-20 18:19:01 +0200

    Keep advancing on the adapter

--- src/main/java/org/integratedmodelling/raster/RasterPlugin.java
+++ src/main/java/org/integratedmodelling/raster/RasterPlugin.java
@@ -1,14 +1,11 @@
 package org.integratedmodelling.raster;
 
+import org.integratedmodelling.klab.extension.KlabComponent;
 import org.pf4j.PluginWrapper;
 
-public class RasterPlugin {//extends KlabComponent {
+public class RasterPlugin extends KlabComponent {
 
     public RasterPlugin(PluginWrapper wrapper) {
-        super();
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Hi, raster!");
+        super(wrapper);
     }
 }
