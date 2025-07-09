76fd9ed4fa3feda3b39fa1d8e1358f553eacfda9
Íñigo Cobián <inigo.cobian@gmail.com>
2025-07-09 16:27:07 +0200

    Rename Plugin class

--- src/main/java/org/integratedmodelling/geospatial/GeospatialPlugin.java
+++ src/main/java/org/integratedmodelling/geospatial/GeospatialPlugin.java
@@ -3,9 +3,9 @@ package org.integratedmodelling.geospatial;
 import org.integratedmodelling.klab.extension.KlabComponent;
 import org.pf4j.PluginWrapper;
 
-public class RasterPlugin extends KlabComponent {
+public class GeospatialPlugin extends KlabComponent {
 
-    public RasterPlugin(PluginWrapper wrapper) {
+    public GeospatialPlugin(PluginWrapper wrapper) {
         super(wrapper);
     }
 }
